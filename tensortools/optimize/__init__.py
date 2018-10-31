980c473e4d76744c028755b421dff4f62fe3c448
Ankit Khambhati <akhambhati@gmail.com>
2018-10-31 16:48:46 -0700

    Added references to ncp_betadiv.py optimize module

--- tensortools/optimize/__init__.py
+++ tensortools/optimize/__init__.py
@@ -7,3 +7,5 @@ from .cp_als import cp_als
 from .mcp_als import mcp_als
 from .ncp_hals import ncp_hals
 from .ncp_bcd import ncp_bcd
+from .ncp_nnls import ncp_nnls
+from .ncp_betadiv import ncp_betadiv
