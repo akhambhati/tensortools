1d7f119e903fdf1685ab03dfccc6354081d349f9
Ankit Khambhati <akhambhati@gmail.com>
2019-04-19 13:34:03 -0700

    Removed probability vectorization

--- tensortools/optimize/ncp_nnlds.py
+++ tensortools/optimize/ncp_nnlds.py
@@ -328,12 +328,6 @@ def model_update(
             # vi) Update the observational component weights
             W[n] *= (neg / pos)**mm_gamma_func(mp['NTF']['beta'])
 
-            ## Constrain the LDS mode to be a probability vector
-            #if n == mp['LDS']['axis']:
-            #    W[mp['LDS']['axis']] = (
-            #            W[mp['LDS']['axis']].T / np.sum(W[mp['LDS']['axis']], axis=1)).T
-
-
             # vii) Update the dynamical state weights
             if (flag_lds):
                 if n == mp['LDS']['axis']:
