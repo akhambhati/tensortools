3a2029dde2ba9d9a10bb0d160ecd2e485a6fd452
Ankit Khambhati <akhambhati@gmail.com>
2021-06-07 01:46:31 -0700

    Adopt tensorly for tensor manipulation'

--- tensortools/tensors.py
+++ tensortools/tensors.py
@@ -1,7 +1,5 @@
-from tensortools.operations import khatri_rao
 import numpy as np
-import scipy as sci
-from copy import deepcopy
+import tensorly as tl
 
 
 class KTensor(object):
@@ -41,26 +39,7 @@ class KTensor(object):
     def full(self):
         """Converts KTensor to a dense ndarray."""
 
-        # Compute tensor unfolding along first mode
-        unf = sci.dot(self.factors[0], khatri_rao(self.factors[1:]).T)
-
-        # Inverse unfolding along first mode
-        return sci.reshape(unf, self.shape)
-
-    def rebalance(self):
-        """Rescales factors across modes so that all norms match.
-        """
-
-        # Compute norms along columns for each factor matrix
-        norms = [sci.linalg.norm(f, axis=0) for f in self.factors]
-
-        # Multiply norms across all modes
-        #lam = sci.multiply.reduce(norms) ** (1/self.ndim)
-        lam = np.prod(norms) ** (1/self.ndim)
-
-        # Update factors
-        self.factors = [f * (lam / fn) for f, fn in zip(self.factors, norms)]
-        return self
+        return tl.cp_tensor.cp_to_tensor((np.ones(self.rank), self.factors))
 
     def permute(self, idx):
         """Permutes the columns of the factor matrices inplace
@@ -81,7 +60,7 @@ class KTensor(object):
         return self.factors[i]
 
     def __setitem__(self, i, factor):
-        factor = sci.array(factor)
+        factor = tl.tensor(factor)
         if factor.shape != (self.shape[i], self.rank):
             raise ValueError('Dimension mismatch in KTensor assignment.')
         self.factors[i] = factor
